78f0ed1735fff643ced8baba57169813f98ebf93
Tatsuya Ogi <t.oginogin@gmail.com>
2014-10-17 09:22:22 +0900

    add Error Page #13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,20 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from Exception, with: :error500
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+
+  private
+
+  def error404(e)
+    render 'error404', status: 404, formats: [:html]
+  end
+
+  def error500(e)
+    logger.error [e, *e.backtrace].join("\n")
+    render 'error500', status: 500, formats: [:html]
+  end
+
   protected
 
   def configure_permitted_parameters
